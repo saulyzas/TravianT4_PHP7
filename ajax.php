197b95838567b271de8bdf72e8045521a5030845
dsteindo <dsteindo@gmx.at>
2019-09-13 13:46:42 +0200

    Fixed Ajax endpoint

--- ajax.php
+++ ajax.php
@@ -1,44 +1,47 @@
 <?php
 if($_GET){
-	include_once ("config/connection.php");
-	include_once ("config/config.php");
-	mysql_connect(SQL_SERVER, SQL_USER, SQL_PASS);
-	mysql_select_db(SQL_DB);
-	switch($_GET['f']) {
-		case 'qst':
-			if (isset($_GET['qact'])){
-				$qact=$_GET['qact'];
-			}else {
-				$qact=null;
-			}
-			if (isset($_GET['qact2'])){
-				$qact2=$_GET['qact2'];
-			}else {
-				$qact2=null;
-			}
-			if (isset($_GET['qact3'])){
-				$qact3=$_GET['qact3'];
-			}else {
-				$qact3=null;
-			}
-			include("Templates/Ajax/quest_core.tpl");		
-		break;
+	
+	if (isset($_GET['f'])) {
+		switch($_GET['f']) {
+			case 'qst':
+				if (isset($_GET['qact'])){
+					$qact=$_GET['qact'];
+				}else {
+					$qact=null;
+				}
+				if (isset($_GET['qact2'])){
+					$qact2=$_GET['qact2'];
+				}else {
+					$qact2=null;
+				}
+				if (isset($_GET['qact3'])){
+					$qact3=$_GET['qact3'];
+				}else {
+					$qact3=null;
+				}
+				include("Templates/Ajax/quest_core.tpl");		
+			break;
+		}
 	}
-	switch($_GET['cmd']) {
-		
-		case 'changeVillageName':
-			$q = "UPDATE " . TB_PREFIX . "vdata SET `name` = '" . $_POST['name'] . "' where `wref` = '" . $_POST['did'] . "'";
-    		mysql_query($q);
-		break;
-		
-		case 'mapLowRes':
-			$x = $_POST['x'];
-			$y = $_POST['y'];
-			$xx = $_POST['width'];
-			$yy = $_POST['height'];
+	if (isset($_GET['cmd'])) {
+		switch($_GET['cmd']) {
 			
-			include("Templates/Ajax/mapscroll.tpl");
-		break;
+			case 'changeVillageName':
+				include_once ("GameEngine/Database.php");
+		
+				$q = "UPDATE " . TB_PREFIX . "vdata SET `name` = '" . $_POST['name'] . "' where `wref` = '" . $_POST['did'] . "'";
+				mysql_query($q);
+			break;
+			
+			case 'mapLowRes':
+				$x = $_POST['x'];
+				$y = $_POST['y'];
+				$xx = $_POST['width'];
+				$yy = $_POST['height'];
+				
+				include("Templates/Ajax/mapscroll.tpl");
+			break;
+		}
 	}
 
 }
